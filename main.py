3c7e19e747c47ebdfe6967cf0025660060f14764
fallnomega <fallnomega@gmail.com>
2023-02-02 12:43:45 -0800

    building out the game

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 import data
 from question_model import Question
 from quiz_brain import QuizBrain
+from art import logo
 
 list_of_questions =[]
 for x in data.question_data:
@@ -9,7 +10,7 @@ for x in data.question_data:
 the_brain = QuizBrain(list_of_questions)
 # print(list_of_questions)
 # print (the_brain.questions_list)
-
+print(logo)
 the_brain.askQuestion()
 
 # selected_question = random.choices(list_of_questions)
