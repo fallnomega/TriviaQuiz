3b35385c586e20b07f62ff987e379a024b7895a3
fallnomega <fallnomega@gmail.com>
2023-02-02 13:43:16 -0800

    building out the game

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import random
-import data
+from data import question_data
 from question_model import Question
 from quiz_brain import QuizBrain
 from art import logo
-
-list_of_questions =[]
-for x in data.question_data:
-    list_of_questions.append(x)
-the_brain = QuizBrain(list_of_questions)
-# print(list_of_questions)
-# print (the_brain.questions_list)
-print(logo)
-the_brain.askQuestion()
-
-# selected_question = random.choices(list_of_questions)
-# selected = Question(selected_question[0]['text'],selected_question[0]['answer'])
-# print(selected.question)
-# print(selected.answer)
 #
-# print ("\n\n\n")
-# print(selected_question.list_of_questions)
-# print(random.choices(selected_question.list_of_questions))
+list_of_questions = []
+for question in question_data:
+    list_of_questions.append(Question(question['text'],question['answer']))
+# for x in list_of_questions:
+#     print (x.question)
+# print(list_of_questions)
+
+the_brain = QuizBrain(list_of_questions)
+# print(list_of_questions.)
+# # print (the_brain.questions_list)
+# # print(logo)
+#
+# game_on = True
+# while game_on:
+#     the_brain.nextQuestion()
+
+
+
--- quiz_brain.py
+++ quiz_brain.py
@@ -1,14 +1,36 @@
-#TODO asking the quesitons
-#TODO checking if the answer was correct
-#TODO checking if we're at the end of the quiz
+# TODO checking if the answer was correct
+# TODO checking if we're at the end of the quiz
 import random
 
 
 class QuizBrain:
-    def __init__(self,q_list):
-        self.question_number =0
-        self.questions_list =q_list
-    def askQuestion(self):
-        self.question_number+=1
-        self.question = random.choices(self.questions_list)
-        print(f"Q{self.question_number}. True or False - {self.question[0]['text']}")
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.questions_list = q_list
+
+    def nextQuestion(self):
+        self.question_number += 1
+        self.index_choice = random.randint(0, len(self.questions_list) - 1)
+        print(self.index_choice)
+        self.question = self.questions_list[int(self.index_choice)]
+        # print(type(self.index_choice))
+        # print(type(self.questions_list))
+        # self.questions_list.remove(9)
+        # print(len(self.questions_list))
+        # print(self.question)
+        # exit()
+        self.answer = input(f"Q{self.question_number}. {self.question['text']} (True/False) - : ").lower()
+        if self.answer != 'true' and self.answer != 'false':
+            print("Please answer with True or False")
+        else:
+            self.checkAnswer()
+    def still_has_questions(self):
+        return self.question_number < len(self.questions_list)
+
+
+    def checkAnswer(self):
+        if self.answer == self.question['answer'].lower():
+            print(f"\nCorrect, the answer is: {self.answer}")
+
+        else:
+            print(f"\nWrong, the correct answer is: {self.question['answer'].lower()}")
